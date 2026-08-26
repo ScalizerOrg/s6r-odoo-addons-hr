a31c22b71a4f3c5aa11c478288e5740560205b87
Morgane Goujon <morgane@scalizer.fr>
2026-08-26 12:40:34 +0200

    [IMP] 28581: improve overtime module with new module which linked leave type to employee category

---
+++ s6r_hr_leave_type_employee_category/wizard/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2026 Scalizer (<https://www.scalizer.fr>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.html).
+
+from . import hr_leave_generate_multi_restriction
+from . import hr_leave_generate_multi_wizard
+from . import hr_leave_allocation_generate_multi_wizard
